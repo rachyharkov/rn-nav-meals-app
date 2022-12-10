22cdcf872c8486c8c7301926b96fe2c224244e3c
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-10 08:54:45 +0700

    implement redux

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
+import { Provider } from "react-redux";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoriteScreen from "./screens/FavoriteScreen";
 import FavoritesContextProvider from "./store/context/favorites-context";
+import { store } from "./store/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator()
@@ -49,7 +51,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light"></StatusBar>
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -73,7 +76,8 @@ export default function App() {
             <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{title: 'About the Meal'}} />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useContext } from "react";
 
 import MealsList from "../components/MealsList/MealsList";
@@ -11,9 +11,32 @@ function FavoriteScreen() {
 
     const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id))
 
+    if(favoriteMeals.length <= 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>No favorite meals found. Start adding some!</Text>
+            </View>
+        )
+    }
+
     return(
         <MealsList items={favoriteMeals} />
     )
 }
 
 export default FavoriteScreen
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+        paddingHorizontal: 20,
+        textAlign: 'center'
+    }
+})
